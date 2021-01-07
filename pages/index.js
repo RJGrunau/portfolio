f14322006704b445b747364b4ca2da55f541797a
Robert Grunau <robertgrunau@gmail.com>
2021-01-07 15:24:03 -0800

    added graphql

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,66 @@
 import Head from 'next/head'
+import { request } from '../libs/datocms'
 import styles from '../styles/Home.module.css'
 import MainLayout from '../components/layout-components/layout/layout'
 
-export default function Home() {
+const HOMEPAGE_QUERY = `
+  query HomePage ($limit: IntType){
+    author {
+      authorPhoto {
+        responsiveImage (imgixParams: { fit: crop, w: 300, h: 300, auto: format }){
+          alt
+          src
+          srcSet
+          title
+          aspectRatio
+          base64
+          bgColor
+          width
+          webpSrcSet
+          height
+        }
+      }
+      authorname
+      jobtitle
+    }
+    allPosts (first: $limit) {
+      excerpt
+      date
+      coverPhoto {
+        alt
+        responsiveImage (imgixParams: { fit: crop, w: 300, h: 300, auto: format }){
+          sizes
+          aspectRatio
+          alt
+          base64
+          bgColor
+          height
+          src
+          srcSet
+          title
+          webpSrcSet
+          width
+        }
+      }
+    }
+  }
+`
+
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: { limit: 3 },
+  })
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default function Home({data}) {
+  console.log(data);
   return (
     <>
       <Head>
