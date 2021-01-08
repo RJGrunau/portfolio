bc270597d3172c39671b95441a61557a7846dbe2
Robert Grunau <robertgrunau@gmail.com>
2021-01-07 21:37:52 -0800

    redo header

--- components/layout-components/header/header.jsx
+++ components/layout-components/header/header.jsx
@@ -1,10 +1,17 @@
+import Link from 'next/link'
 import styles from './header.module.css'
 import Logo from "./logo";
 
 const SiteHeader = () => {
     return ( 
         <header className={styles.header}>
-            <Logo/>
+            <div className={styles.logoHolder}>
+                <Link href="/">
+                    <a className="anchor"> 
+                        <Logo/>
+                    </a>
+                </Link>
+            </div>
         </header>
     );
 }
