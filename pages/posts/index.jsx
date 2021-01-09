1f48fecc157bf68571ab6868a5bb9d8af801271e
Robert Grunau <robertgrunau@gmail.com>
2021-01-08 20:31:06 -0800

    posts on page

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,11 +1,51 @@
 import styles from '../../styles/posts.module.css'
+import { request } from '../../libs/datocms'
+import Link from 'next/link'
+import Layout from '../../components/layout-components/layout/layout'
 
 
-const PostsPage = () => {
+const POSTSPAGE_QUERY = `
+    query PostsPage {
+        allPosts {
+            id
+            slug
+            date
+            title
+        }
+    }
+`
+export async function getStaticProps(){
+    const data = await request({
+        query: POSTSPAGE_QUERY
+    })
+
+    return{
+        props: {
+           data,
+        }
+    }
+}
+const PostsPage = ({data}) => {
+    
     return ( 
-        <section class={styles.postSection}>
-            hi
-        </section>
+        <Layout>
+            <section className={styles.postSection}>
+                <ol className={styles.ol}>
+                    {data.allPosts.map(p => (
+                        <li className={styles.li} key={p.id}>
+                            <Link href={`/${p.slug}`}>
+                                <a className="link">
+                                    <div>
+                                        {p.title}
+                                    </div>
+                                </a>
+                            </Link>
+                            <time className={styles.time}>{p.date}</time>
+                        </li>
+                    ))}
+                </ol>
+            </section>
+        </Layout>
     );
 }
  
