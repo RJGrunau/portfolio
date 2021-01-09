299ae67982ee9a28a70ea99fe22221372caed4b9
Robert Grunau <robertgrunau@gmail.com>
2021-01-08 20:39:14 -0800

    set up for post slugs

---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/post-slug.module.css'
+
+
+const PostSlug = () => {
+    return ( 
+        <article className={styles.article}>
+            
+        </article>
+    );
+}
+ 
+export default PostSlug;
