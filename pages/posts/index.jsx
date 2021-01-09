8638fcf46377b30314c1c5fba80b0a4c13797780
Robert Grunau <robertgrunau@gmail.com>
2021-01-08 19:53:20 -0800

    trying to add posts page

---
+++ pages/posts/index.jsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/posts.module.css'
+
+
+const PostsPage = () => {
+    return ( 
+        <section class={styles.postSection}>
+            hi
+        </section>
+    );
+}
+ 
+export default PostsPage;
