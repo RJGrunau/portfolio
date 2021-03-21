d07ec70c577f1aec1387e4ca76409514ae768c2e
Robert Grunau <rgrunau@liveperson.com>
2021-03-21 15:28:15 -0700

    start gallery block

---
+++ components/home-page/gallery-block/gallery-block.jsx
@@ -0,0 +1,12 @@
+import styles from './gallery-block.module.css';
+import { Image } from 'react-datocms';
+
+const GalleryBlock = ({pageGallery}) => {
+    return ( 
+        <section className={styles.section}>
+
+        </section>
+     );
+}
+ 
+export default GalleryBlock;
