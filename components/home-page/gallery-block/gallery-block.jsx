1bbadc5aca72d214336592d93705447787fa87fb
Robert Grunau <rgrunau@liveperson.com>
2021-03-21 15:37:04 -0700

    testing

--- components/home-page/gallery-block/gallery-block.jsx
+++ components/home-page/gallery-block/gallery-block.jsx
@@ -1,10 +1,18 @@
+import React, {useState, useEffect} from 'react';
 import styles from './gallery-block.module.css';
 import { Image } from 'react-datocms';
 
 const GalleryBlock = ({pageGallery}) => {
+    const [gallery, setGallery] = useState(null);
+    const [randNumber, setrandNumber] = useState(null);
+
+    useEffect(() => {
+        const photos = pageGallery;
+        setGallery(photos);
+    })
     return ( 
         <section className={styles.section}>
-
+            test
         </section>
      );
 }
