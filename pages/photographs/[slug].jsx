21c5ec9a1fc111d85879a88b0864ee046efc4ae2
Robert Grunau <rgrunau@liveperson.com>
2021-03-21 20:42:56 -0700

    first attemp

---
+++ pages/photographs/[slug].jsx
@@ -0,0 +1,63 @@
+import markdownToHtml from '../../libs/markdownToHTML';
+
+import { Image } from 'react-datocms'
+import MainLayout from '../../components/layout-components/layout/layout';
+import request from 'graphql-request';
+
+const GALLERIESBYSLUG_QUERY = `
+    query GalleryBySlug ($slug: String){
+        photograph(filter: {slug: {eq: $slug}}) {
+            id 
+            slug
+            title
+            gallery {
+                responsiveImage {
+                    alt
+                    aspectRatio
+                    base64
+                    height
+                    sizes
+                    src
+                    srcSet
+                    title
+                    webpSrcSet
+                    width
+                }
+            }
+        }
+    }
+`
+
+export async function getStaticPaths(){
+    const data = await request({query: `{allPhotographs {slug}}`})
+
+    return {
+        paths: data.allPhotographs.map((gallery) => `/photographs/${gallery.slug}`),
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}){
+    const data = await request({
+        query: GALLERIESBYSLUG_QUERY,
+        variables: {
+            slug: params.slug
+        }
+    })
+
+    return {
+        props: {
+            data,
+        }
+    }
+}
+
+const Gallery = ({data}) => {
+    return (  
+        <MainLayout>
+            {data.title}
+        </MainLayout>
+    );
+}
+ 
+export default Gallery;
--- pages/photographs/index.jsx
+++ pages/photographs/index.jsx
@@ -1,7 +1,8 @@
 import { request } from '../../libs/datocms';
+import { Image } from 'react-datocms';
 import styles from '../../styles/photographs.module.css'
 import MainLayout from '../../components/layout-components/layout/layout';
-import { Image } from 'react-datocms';
+import Link from 'next/link';
 
 const PHOTOGRAPHS_QUERY = `
     query PhotographsPage {
@@ -40,19 +41,20 @@ export async function getStaticProps() {
 
 const Photographs = ({data}) => {
     const allGalleries = data.allPhotographs
-    console.log(allGalleries.gallery);
     return ( 
         <MainLayout>
             <section className={styles.galleriesSection}>
                 <ul className={styles.galleryList}>
                     {allGalleries.map((gall,i) => (
                         <li>
+                            <Link href={`photographs/${gall.slug}`}>
                                 <div className={styles.galleryCard} key={i}>
-                                    <div>
+                                    <div className={styles.photoHolder}>
                                         <Image data={gall.gallery[0].responsiveImage}/>
                                     </div>
                                     <h2>{gall.title}</h2>
                                 </div>
+                            </Link>
                         </li>
                     ))}
                 </ul>
