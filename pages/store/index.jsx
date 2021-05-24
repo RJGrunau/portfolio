764f98041d2f479c24a33cea95855b4f25503fca
Robert Grunau <robertgrunau@hey.com>
2021-05-23 22:19:00 -0700

    get products

--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,16 +1,48 @@
 import {useState, useEffect} from 'react'
-
+import { request } from '../../libs/datocms'
 import MainLayout from '../../components/layout-components/layout/layout'
 
 import styles from "../../styles/products-page.module.css"
 import Product from '../../components/products/indv-product/product'
 
+const SHOPPAGE_QUERY = `
+    query ShopPage {
+        allProducts {
+            productImages {
+              responsiveImage {
+                alt
+                aspectRatio
+                base64
+                height
+                sizes
+                src
+                srcSet
+                title
+                webpSrcSet
+                width
+              }
+            }
+            title
+            productId
+            price
+            inventory
+        }
+    }
+`
+export async function getStaticProps() {
+   const data = await request({
+       query: SHOPPAGE_QUERY
+   });
 
-export const getServerSideProps = async (context) => {
-   
+   return{
+       props: {
+           data,
+       },
+   }
 }
 
-const StorePage = (props) => {
+const StorePage = ({data}) => {
+    console.log(data)
     return(
         <MainLayout title="Store">
            STORE
